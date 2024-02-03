7e5a5b9daef9b62004f748d92e32dab80cd99cb1
Teset13118 <109953575+Teset13118@users.noreply.github.com>
2024-02-03 22:09:52 +0700

    finally

--- swap.js
+++ swap.js
@@ -1,6 +1,32 @@
 function swap(){
     var box1 = document.getElementById("bigbox1");
     var box2 = document.getElementById("bigbox2");
-
-    
+    var button = document.getElementById("button");
+    if(button.value == "Swap to CSS"){
+        box1.style.display = "none";
+        box2.style.display = "flex";
+        button.value = "Swap to HTML";
+        button.innerHTML = "Swap to HTML";
+    }else if(button.value == "Swap to HTML"){
+        box1.style.display = "flex";
+        box2.style.display = "none";
+        button.value = "Swap to CSS";
+        button.innerHTML = "Swap to CSS";
+    }
+}
+function navswaphtml(){
+    var box1 = document.getElementById("bigbox1");
+    var box2 = document.getElementById("bigbox2");
+    box1.style.display = "flex";
+    box2.style.display = "none";
+    button.value = "Swap to CSS";
+    button.innerHTML = "Swap to CSS";
+}
+function navswapcss(){
+    var box1 = document.getElementById("bigbox1");
+    var box2 = document.getElementById("bigbox2");
+    box1.style.display = "none";
+    box2.style.display = "flex";
+    button.value = "Swap to HTML";
+    button.innerHTML = "Swap to HTML";
 }
